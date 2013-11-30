79c9f82f7a3095a290fd67db495258e62b443eb2
nullproduction <nullproduction@gmail.com>
2013-11-30 15:31:09 +0400

    Initial Commit

---
+++ PopupViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopupViewController
+//
+//  Created by mezhevikin on 30.11.13.
+//  Copyright (c) 2013 Mezhevikin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
